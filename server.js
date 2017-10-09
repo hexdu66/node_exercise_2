af32513abc4eb6098a5c95c70faaf01380bdaff3
Arnaud Soler <arnaus17@student.uia.no>
2017-10-09 14:15:18 +0200

    publishing my app

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
+
+app.use(express.static('public'))
+
+app.get('/greet', function (req, res) {
+    var myGreetings = new Array();
+
+    myGreetings[0] = "Hi !";
+    myGreetings[1] = "Hello !";
+    myGreetings[2] = "Bonjour !";
+    myGreetings[3] = "Hola !";
+    myGreetings[4] = "Hei hei !";
+    myGreetings[5] = "Hallo !";
+    myGreetings[6] = "Salut !";
+
+    console.log(myGreetings[Math.floor((Math.random() * 6))]);
+})
+
+app.get('/greet/:id', function (req, res) {
+    var myGreetings = new Array();
+
+    myGreetings[0] = "Hi !";
+    myGreetings[1] = "Hello !";
+    myGreetings[2] = "Bonjour !";
+    myGreetings[3] = "Hola !";
+    myGreetings[4] = "Hei hei !";
+    myGreetings[5] = "Hallo !";
+    myGreetings[6] = "Salut !";
+
+    console.log(myGreetings[Math.floor((Math.random() * 6))] + " " +req.params.id);
+})
