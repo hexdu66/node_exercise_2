d1484490def59fcb2a83326707071f8bd121e990
Arnaud Soler <arnaus17@student.uia.no>
2017-10-10 14:19:52 +0200

    another commit

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.listen(process.env.PORT || 8080, function () {
 
 app.use(express.static('public'))
 
-app.get('/greet', function (req, res) {
-    var myGreetings = new Array();
+app.get('/greet/:id?', function (req, res) {
+    var myGreetings = [];
 
     myGreetings[0] = "Hi !";
     myGreetings[1] = "Hello !";
@@ -21,20 +21,10 @@ app.get('/greet', function (req, res) {
     myGreetings[4] = "Hei hei !";
     myGreetings[5] = "Hallo !";
     myGreetings[6] = "Salut !";
-
-   res.send(myGreetings[Math.floor((Math.random() * 6))]);
+    
+    if(!req.params.id)
+        res.send(myGreetings[Math.floor((Math.random() * 6))]);
+    else
+        res.send(myGreetings[Math.floor((Math.random() * 6))] + " " +req.params.id);
 })
-
-app.get('/greet/:id', function (req, res) {
-    var myGreetings = new Array();
-
-    myGreetings[0] = "Hi !";
-    myGreetings[1] = "Hello !";
-    myGreetings[2] = "Bonjour !";
-    myGreetings[3] = "Hola !";
-    myGreetings[4] = "Hei hei !";
-    myGreetings[5] = "Hallo !";
-    myGreetings[6] = "Salut !";
-
-    res.send(myGreetings[Math.floor((Math.random() * 6))] + " " +req.params.id);
-})
+   
